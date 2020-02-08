d7c7bb842eebd1078920b26aa2bc4fd623712486
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-02-08 14:43:04 -0300

    Remove general configuration for `nvm` and `vim`
    They were moved to specific place by host.

--- src/bella/aliases.sh
+++ src/bella/aliases.sh
@@ -15,13 +15,6 @@ alias xtris='/usr/bin/xtris -n $LOGNAME'
 
 alias perl-rename='perl-rename -i'
 
-# 2014.11.05 let's try NeoVim for a while
-# 2014.11.09 let's keep using vim by default until I get all broken stuff in nvim sorted out
-# 2019.04.23 let's try again
-alias vi='/usr/bin/nvim -p'
-alias oldvi='/usr/bin/vi'
-alias fastvim='/usr/bin/nvim --noplugin +'
-
 # let me on current pwd after exiting mc
 alias mc='. /usr/lib/mc/mc-wrapper.sh'
 
