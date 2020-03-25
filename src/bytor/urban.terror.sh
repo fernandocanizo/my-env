9d7c5e409bbe7ba36e1ffd8b1673e1c96ca9326f
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-03-25 09:10:00 -0300

    Move around some files
    This is part of the move into a way to have a good starting point for
    servers as for my machines.
    
    So everything that can go into a server, goes into `src/common`, then
    particular stuff to a machine, goes into that machine folder:
    `src/$HOST`. Stuff I don't want automatically loaded goes into
    `src/other` and can be linked from other places when I want it
    automatically loaded on that particular machine.

---
+++ src/bytor/urban.terror.sh
@@ -0,0 +1,2 @@
+
+alias ut='/opt/urbanterror/urbanterror-m9.x86_64'
---
+++ src/other/vi.sh
@@ -0,0 +1,6 @@
+# 2014.11.05 let's try NeoVim for a while
+# 2014.11.09 let's keep using vim by default until I get all broken stuff in nvim sorted out
+# 2019.04.23 let's try again
+alias vi='/usr/bin/nvim -p'
+alias oldvi='/usr/bin/vi'
+alias fastvim='/usr/bin/nvim --noplugin +'
