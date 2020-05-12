5b541ed3788acac254ca0f8b3bce289f57c3b936
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-05-12 08:24:55 -0400

    Stop restoring sound level on every new terminal

--- src/bella/restore.alsamixer.sh
+++ src/bella/restore.alsamixer.sh
@@ -4,4 +4,7 @@
 # to save them in the first place use:
 # alsactl store -f /home/flc/.config/asoundrc
 # after you have configured it to your taste
-alsactl -f ~/.config/asoundrc restore 2>/dev/null
+# 2020.04.16 es muy molesto que se me baje el sonido cada vez que arranco una terminal
+# esto debería quedar en la conf de login, no la de cada vez que inicio un shell
+# no recuerdo si ese era .bashrc o .bash_profile
+#alsactl -f ~/.config/asoundrc restore 2>/dev/null
