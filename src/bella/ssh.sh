5c23220528bd5b52ebd0f7c04d01d42c63c303b7
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-06-08 12:08:29 -0400

    Load keys conditionally
    So we don't have messages about addign duplicated keys each time we
    start a terminal

--- src/bella/ssh.sh
+++ src/bella/ssh.sh
@@ -1,3 +1,10 @@
 # Add daily needed ssh keys
+# conditionally load daily used keys
 
-ssh-add ~/.ssh/github_bella
+# github
+$(ssh-add -l | grep -q 'SHA256:CE5a3EGQk6a0hdUFkfUEuMsdHWgkoThPbPm9uV9fJts') || ssh-add ~/.ssh/github_bella
+
+# Esta key se está agregando sola no sé cómo
+# aws
+#$(ssh-add -l | grep -q 'SHA256:vWp8lGJrfIHwowO5MT4UBuso/hrlj23sP3/7LyV/tCg') || ssh-add ~/.ssh/flc-csw.pem
+
