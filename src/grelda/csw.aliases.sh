cb599b8f705fa75f7b5345a48f86bd6db4ec7b98
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-06-15 10:53:57 -0400

    Add specific start up scripts for `grelda`

---
+++ src/grelda/csw.aliases.sh
@@ -0,0 +1,9 @@
+alias cd.maps='cd /my/soft/csw/webgis/maps'
+
+alias cd.fdd='cd /my/soft/csw/fuentes.de.datos/fdd'
+
+
+# Prefactibilidad DB
+alias pref-pgcli='pgcli --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com -U csw test'
+alias pref-psql='psql --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com -U csw test'
+alias pref-activity='pg_activity -U csw -d test -h prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com --rds'
---
+++ src/grelda/load.github.key.sh
@@ -0,0 +1,3 @@
+# Add github key
+$(ssh-add -l | grep -q 'SHA256:CE5a3EGQk6a0hdUFkfUEuMsdHWgkoThPbPm9uV9fJts') || ssh-add ~/.ssh/github_bella
+
