2377087e2482698610f189e6dbca648186356de5
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-06-18 17:02:28 -0400

    Alias for prefactibilidad on `grelda`

--- src/grelda/csw.aliases.sh
+++ src/grelda/csw.aliases.sh
@@ -7,3 +7,5 @@ alias cd.fdd='cd /my/soft/csw/fuentes.de.datos/fdd'
 alias pref-pgcli='pgcli --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com -U csw test'
 alias pref-psql='psql --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com -U csw test'
 alias pref-activity='pg_activity -U csw -d test -h prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com --rds'
+
+alias cd.pref='cd /my/soft/csw/reporte.ambiental/prefactibilidad'
