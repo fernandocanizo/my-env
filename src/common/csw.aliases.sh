d1eff33c1e35e21d58ad17c71cccc1cb42e0cba4
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-08-28 14:29:59 -0400

    Move csw aliases to common scripts
    And link it from `grelda` and `bytor`

---
+++ src/common/csw.aliases.sh
@@ -0,0 +1,13 @@
+alias cd.maps='cd /my/soft/csw/webgis/maps'
+
+alias cd.fdd='cd /my/soft/csw/fuentes.de.datos/fdd'
+alias cd.eecc='cd /my/soft/csw/crawlers/eecc'
+alias cd.sea-crawler='cd /my/soft/csw/crawlers/sea-crawler'
+
+
+# Prefactibilidad DB
+alias pref-pgcli='pgcli --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com -U csw test'
+alias pref-psql='psql --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com -U csw test'
+alias pref-activity='pg_activity -U csw -d test -h prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com --rds'
+
+alias cd.pref='cd /my/soft/csw/reporte.ambiental/prefactibilidad'
