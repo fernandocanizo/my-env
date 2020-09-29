61ce5e95b600e115bcf3e97aa68e5ed8ec867de4
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-09-29 16:09:22 -0300

    More CSW aliases

--- src/common/csw.aliases.sh
+++ src/common/csw.aliases.sh
@@ -3,6 +3,7 @@ alias cd.maps='cd /my/soft/csw/webgis/maps'
 alias cd.fdd='cd /my/soft/csw/fuentes.de.datos/fdd'
 alias cd.eecc='cd /my/soft/csw/crawlers/eecc'
 alias cd.sea-crawler='cd /my/soft/csw/crawlers/sea-crawler'
+alias cd.specietor='cd /my/soft/csw/nviro/specietor'
 
 
 # Prefactibilidad DB
@@ -11,6 +12,7 @@ alias pref-psql='psql --host prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaw
 alias pref-activity='pg_activity -U csw -d test -h prefactibilidad.cy3tepzkndt3.us-west-2.rds.amazonaws.com --rds'
 
 alias cd.pref='cd /my/soft/csw/reporte.ambiental/prefactibilidad'
+alias cd.presences='cd /my/soft/csw/fuentes.de.datos/presences/'
 
 # SEA
 alias cd.sea-enslaver-back='cd /my/soft/csw/sea-enslaver/sea-digitizers-back'
