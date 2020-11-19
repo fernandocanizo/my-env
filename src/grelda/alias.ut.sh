188b3facbf01325cd894a18f2f28cb5dd8b91005
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-11-19 12:44:52 -0300

    Add Urban Terror alias to play directly in scrim mode

--- src/grelda/alias.ut.sh
+++ src/grelda/alias.ut.sh
@@ -1,2 +1,6 @@
 # alias para el ut que me pasó Eyemaster
 alias ut='~/ut/urbanterror-m9.x86_64'
+
+# ut en modo scrim. Usar antes el comando utconn para reconfigurar el archivo de conexión
+# scrim.cfg
+alias uts='ut +exec scrim'
