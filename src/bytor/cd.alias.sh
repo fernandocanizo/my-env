3eae6de2b7b13af95e9fc82ea970321cada4b9df
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2021-05-06 18:50:08 -0400

    Add cd alias to my fernandocanizo.dev repo

--- src/bytor/cd.alias.sh
+++ src/bytor/cd.alias.sh
@@ -1,3 +1,5 @@
 alias cd.py4at='cd /my/soft/learning/2021.04.23,py4at/'
 
 alias cd.neps='cd /my/soft/node-express-postgres-scaffold'
+
+alias cd.flc.dev='cd /my/soft/0.my.websites/fernandocanizo.dev'
