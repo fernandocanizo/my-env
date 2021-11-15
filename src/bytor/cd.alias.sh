fd3a703cd82bd58b41e221023d3e6bf1e98bf6b7
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2021-11-15 17:19:46 -0300

    Replace cd alias for node express scaffolder

--- src/bytor/cd.alias.sh
+++ src/bytor/cd.alias.sh
@@ -1,7 +1,7 @@
 alias cd.py4at='cd /my/soft/0.learning/2021.04.23,py4at/'
 alias cd.pcc='cd /my/soft/0.learning/2021.06.21,pcc'
 
-alias cd.neps='cd /my/soft/node-express-postgres-scaffold'
+alias cd.neps='cd /my/soft/neps'
 
 alias cd.flc.dev='cd /my/soft/0.my.websites/fernandocanizo.dev'
 
