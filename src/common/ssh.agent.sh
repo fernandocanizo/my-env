b716190b8f2c0ef0e5f1c7767190f665d7f6fd4a
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2021-11-16 12:44:35 -0300

    Add distro recognition to define how to load ssh-agent

---
+++ src/common/ssh.agent.sh
@@ -0,0 +1,24 @@
+# Find out which system is this
+
+distro=$(lsb_release -a 2> /dev/null | grep 'Distributor ID' | awk '{print $NF}')
+
+if [[ ${distro} = 'ManjaroLinux' ]]; then
+  if ! pgrep -u $USER ssh-agent > /dev/null; then
+    # start an agent if we didn't find any
+    ssh-agent >| ~/.ssh-agent-thing
+  fi
+
+  if [[ "$SSH_AGENT_PID" == "" ]]; then
+    # get running agent
+    eval $(<~/.ssh-agent-thing)
+  fi
+
+elif [[ ${distro} = 'Ubuntu' ]]; then
+  # On Ubuntu system has an already running agent, so
+  # Get ssh-agent data to be able to add keys
+  export SSH_AUTH_SOCK=$(ps axu | grep ssh-agent | grep -v grep | head -n 1 | awk  '{print $NF}')
+  export SSH_AGENT_PID=$(pgrep -u $USER ssh-agent)
+fi
+
+
+
