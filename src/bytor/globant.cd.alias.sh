123fa1ddf4eb85eb0bdacb52a096091dc340f359
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2021-11-24 11:31:36 -0300

    New Globant cd aliases

--- src/bytor/globant.cd.alias.sh
+++ src/bytor/globant.cd.alias.sh
@@ -1,1 +1,3 @@
 alias cd.cmss='cd /home/flc/g/soft/univision/channel-metadata-search-syncing'
+alias cd.cma='cd /home/flc/g/soft/univision/content-metadata-adapter'
+alias cd.csa='cd /home/flc/g/soft/univision/content-search-api'
