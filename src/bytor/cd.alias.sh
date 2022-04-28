6933618c5774f08a82acf506fa7239f6c81d6182
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-04-28 16:29:16 -0400

    Add new cd alias for `bytor`

--- src/bytor/cd.alias.sh
+++ src/bytor/cd.alias.sh
@@ -6,3 +6,10 @@ alias cd.neps='cd /my/soft/neps'
 alias cd.flc.dev='cd /my/soft/0.my.websites/fernandocanizo.dev'
 
 alias cd.snake='cd /my/soft/0.games/2017.05.16,coding.snake.in.4.minutes.30.seconds.with.gamkedo'
+
+alias cd.javascript.allonge='cd /my/soft/0.learning/0.books/2022.01.01,javascript.allonge/'
+
+# Globant
+alias cd.dhl='cd /home/flc/g/proyectos/2022.03,dhl/soft'
+alias cd.dhl.core='cd /home/flc/g/proyectos/2022.03,dhl/soft/core'
+alias cd.dhl.web='cd /home/flc/g/proyectos/2022.03,dhl/soft/web'
