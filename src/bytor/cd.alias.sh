eed4e593a300edf51406994d0acc1323a0528406
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-08-18 13:49:29 -0400

    Remove Globant cd aliases

--- src/bytor/cd.alias.sh
+++ src/bytor/cd.alias.sh
@@ -8,8 +8,3 @@ alias cd.flc.dev='cd /my/soft/0.my.websites/fernandocanizo.dev'
 alias cd.snake='cd /my/soft/0.games/2017.05.16,coding.snake.in.4.minutes.30.seconds.with.gamkedo'
 
 alias cd.javascript.allonge='cd /my/soft/0.learning/0.books/2022.01.01,javascript.allonge/'
-
-# Globant
-alias cd.dhl='cd /home/flc/g/proyectos/2022.03,dhl'
-alias cd.dhl.core='cd /home/flc/g/proyectos/2022.03,dhl/soft/core'
-alias cd.dhl.web='cd /home/flc/g/proyectos/2022.03,dhl/soft/web'
