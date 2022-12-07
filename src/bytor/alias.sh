e428031d0bc3f1f49f8ffac741e7755e131319d6
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2022-12-07 15:00:11 -0300

    Add alias for `tomato-git` package
    The binary had an awful long name

--- src/bytor/alias.sh
+++ src/bytor/alias.sh
@@ -4,3 +4,6 @@ alias dst='dropbox-cli status'
 alias pim='pass insert -m '
 alias pe='pass edit '
 alias pgp='pass git push'
+
+# please alias that long and awful name
+alias tomato='/usr/bin/com.github.tomatoers.tomato'
