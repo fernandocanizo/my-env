055ba780220e725319c835166cfac132dcd94f04
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-05-05 09:43:12 -0400

    Remove Globant ssh key references

--- src/bytor/ssh.keys.sh
+++ src/bytor/ssh.keys.sh
@@ -1,3 +1,1 @@
 ssh-add -l | grep -q 'ZI9ddkjS81nw2s1bvZ3CF8m31Jc6SlOV4xxRq3AFNOQ' || ssh-add -q ${HOME}/.ssh/bytor-github-personal-rsa
-
-ssh-add -l | grep -q 'FU0vKu/vlZUhfGmHfv8xb6IgTrz1vw4jQYRbyk4P4yo' || ssh-add -q ${HOME}/.ssh/bytor-github-globant-ed25519-no-pass
