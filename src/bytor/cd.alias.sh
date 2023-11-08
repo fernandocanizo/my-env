32e8da39caf5d523c92d5036074835a78a3a862d
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-08 09:36:41 -0300

    Update tor browser downloads path on alias

--- src/bytor/cd.alias.sh
+++ src/bytor/cd.alias.sh
@@ -10,7 +10,7 @@ alias cd.snake='cd /my/soft/0.games/2017.05.16,coding.snake.in.4.minutes.30.seco
 alias cd.javascript.allonge='cd /my/soft/0.learning/0.books/2022.01.01,javascript.allonge/'
 
 # I never recall where does Tor Browser save downloads
-alias cd.torbrowser='cd /home/flc/.local/share/torbrowser/tbb/x86_64/tor-browser_en-US/Browser/Downloads'
+alias cd.torbrowser='cd /home/flc/.local/share/torbrowser/tbb/x86_64/tor-browser/Browser/Downloads'
 
 alias cd.dgb='cd /home/flc/my/diaz-garage-back'
 alias cd.dgf='cd /home/flc/my/diaz-garage-front'
