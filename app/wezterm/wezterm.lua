36477d03ee62782e875daf6589aa10ad6ab31bab
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-28 08:46:45 -0300

    Add tab renaming shortcut to Wezterm
    Now I can fully switch from Urxvt. Maybe I'll check how to change the
    tab background color too.

--- app/wezterm/wezterm.lua
+++ app/wezterm/wezterm.lua
@@ -86,7 +86,24 @@ config.keys = {
     mods = 'CTRL|SHIFT|ALT',
     key = 'h',
     action = wa.SplitHorizontal({domain="CurrentPaneDomain"})
-  }
+  },
+  -- tab renaming
+  {
+    mods = 'SHIFT',
+    key = 'UpArrow',
+    action = wa.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
+  -- end tab renaming
 }
 
 -- ALT + number tab switching
