9310c4e2218d20ef88812fa3591bcdd70327cecc
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2023-11-28 18:52:27 -0300

    Westerm: active tab color, font size, colorscheme
    - colorscheme: try again 'Batman', I don't know why I switched and it
      looks cool. I can spot my cursor from a mile :P
    - increase font size a notch.
    - FINALLY: got active tab coloring, now I can happily switch from Urxvt
      and work more under Wezterm.

--- app/wezterm/wezterm.lua
+++ app/wezterm/wezterm.lua
@@ -17,23 +17,53 @@ wezterm.log_error('Home ' .. wezterm.home_dir)
 -- didn't work, I'm gonna leave it here to check if I need to relaunch wezterm
 -- from scratch
 
--- For example, changing the color scheme:
--- config.color_scheme = 'Batman'
-
 -- numbers and strings appear in black, which is hard to see with the dark gray background
 -- config.color_scheme = 'Solarized Darcula'
 
 -- pretty sane, but had a hard time reading the path information from my prompt
 -- config.color_scheme = '3024 (dark) (terminal.sexy)'
 
-config.color_scheme = 'Adventure'
+-- used this for a while and wanted to try 'Batman' again, cause I don't know why
+-- I dropped it. But 'Adventure' is fine
+-- config.color_scheme = 'Adventure'
+config.color_scheme = 'Batman'
 
+-- Note (from documentation): the color_scheme option takes precedence over the colors section, and
+-- is mutually exclusive with it. Choose one or use
+-- `wezterm.color.get_default_colors()` and explicitly merge them.
+-- However it seems it doesn't collide with the tab bar colors
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    background = '#0b0022',
+
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      -- The color of the background area for the tab
+      bg_color = '#CE71FE',
+      -- The color of the text for the tab
+      fg_color = '#000000',
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Bold',
+    },
+
+    -- The new tab button that let you create new tabs
+    new_tab = {
+      bg_color = '#ff1032',
+      fg_color = '#000000',
+    },
+  },
+}
 
 -- Spawn a xonsh shell in login mode
 -- config.default_prog = { '/usr/bin/env', 'xonsh' }
 
 config.font = wezterm.font 'Terminess Nerd Font Mono'
-config.font_size = 16.0
+config.font_size = 20.0
 
 local wa = wezterm.action -- just a convenience alias
 config.keys = {
