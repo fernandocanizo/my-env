a951437b0fd38312cd7fd9c5a78e1011762e58aa
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-03-26 12:20:00 -0300

    nvm: remove message about "prefix" option
    Make nvm not complain about:
    `"nvm is not compatible with the npm config "prefix" option`
    
    Anyway, I disabled `nvm` from `bytor` sometime ago, this is just for
    cleaning repository purposes.

--- src/other/nvm.sh
+++ src/other/nvm.sh
@@ -1,3 +1,7 @@
+# Avoid message:
+# "nvm is not compatible with the npm config "prefix" option"
+npm config delete prefix
+
 export NVM_DIR="$HOME/.config/nvm"
 # load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
