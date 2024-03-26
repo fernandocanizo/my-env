76d81bac7a032d4e2ccb95b886e1cebbebb1e2e3
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-03-26 13:46:03 -0300

    Attempt to fix bash loaders
    Got a pretty sane environment, but still some things are odd.

--- src/bytor/env.sh
+++ src/bytor/env.sh
@@ -1,11 +1,4 @@
-export QT_QPA_PLATFORMTHEME="qt5ct"
-export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
-#
-# fix "xdg-open fork-bomb" export your preferred browser from here
-export BROWSER=/usr/bin/brave
-
-export VOLTA_HOME="$HOME/.volta"
-export PATH="$VOLTA_HOME/bin:$PATH"
+export PATH=~/bin:~/Dropbox/bin:${PATH}:~/.mix/escripts
 
 export GIT_TEMPLATE_DIR="${XDG_CONFIG_HOME}/git/template"
 
@@ -13,4 +6,3 @@ export GIT_TEMPLATE_DIR="${XDG_CONFIG_HOME}/git/template"
 export MANPATH="/home/flc/.linuxbrew/share/man:$MANPATH"
 export INFOPATH="/home/flc/.linuxbrew/share/info:$INFOPATH"
 
-export PATH=~/bin:${PATH}
