dd7a146596eb45a180ce224350fef7aac86c333b
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-06-28 14:04:53 -0300

    Add completions for `pnpm` v9, remove old ones
    From v9 onward, `pnpm` changed the way to get completions.

--- src/other/pnpm.sh
+++ src/other/pnpm.sh
@@ -3,8 +3,3 @@ case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
 esac
-
-# completions
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/bash/__tabtab.bash ] && . ~/.config/tabtab/bash/__tabtab.bash || true
