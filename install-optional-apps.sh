3884e0d260f1359d37bb5bdd3f7f0f7838ce4ff6
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-12-03 11:19:23 -0300

    Change optional installer to one command per line
    This way I can easily comment them to install by groups or one-by-one.
    Problem: now it ask on every app to be installed, but it was exploding
    on one of them and making me exit my Sway session completely, so this is
    the less of two evils.

--- install-optional-apps.sh
+++ install-optional-apps.sh
@@ -1,49 +1,44 @@
-yay -S \
-  bash-language-server \
-  bat \
-  brave-bin \
-  chromium \
-  colordiff \
-  convmv \
-  dictd \
-  discord \
-  docker \
-  docker-compose \
-  dog \
-  dos2unix \
-  dosfstools \
-  dropbox \
-  dropbox-cli \
-  encfs \
-  evince \
-  feh \
-  flameshot \
-  gbt \
-  gedit \
-  gitg \
-  guake \
-  jhead \
-  jq \
-  lftp \
-  lshw \
-  lsof \
-  mc \
-  meld \
-  neovim \
-  nmap \
-  opera \
-  p7zip \
-  pacman-contrib \
-  perl-rename \
-  pg_activity \
-  pgcli \
-  pmount \
-  postgresql \
-  qbittorrent \
-  ripgrep \
-  ripgrep-all \
-  shellcheck \
-  telegram-desktop \
-  unrar \
-  xclip \
-  zip \
+# yay -S bash-language-server
+# yay -S bat
+# yay -S brave-bin
+# yay -S chromium
+# yay -S colordiff
+# yay -S convmv
+# yay -S dictd
+# yay -S discord
+# yay -S docker
+# yay -S docker-compose
+# yay -S dog
+# yay -S dos2unix
+# yay -S dosfstools
+# yay -S dropbox-cli
+# yay -S encfs
+# yay -S evince
+# yay -S feh
+# yay -S flameshot
+# yay -S gbt
+# yay -S gedit
+# yay -S gitg
+# yay -S jhead
+# yay -S jq
+# yay -S lftp
+# yay -S lshw
+# yay -S lsof
+# yay -S mc
+# yay -S meld
+# yay -S nmap
+# yay -S opera
+# yay -S p7zip
+# yay -S pacman-contrib
+# yay -S perl-rename
+# yay -S pgcli
+# yay -S pmount
+# yay -S postgresql
+# yay -S qbittorrent
+# yay -S ripgrep
+# yay -S ripgrep-all
+# yay -S shellcheck
+# yay -S telegram-desktop
+# yay -S unrar
+# yay -S xclip
+# yay -S zip
