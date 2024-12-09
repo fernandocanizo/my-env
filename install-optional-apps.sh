6ecbaa934c6bd8c43b902a2c6163117bc7653591
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-12-09 11:42:49 -0300

    optional apps: uncomment and add mongo tools

--- install-optional-apps.sh
+++ install-optional-apps.sh
@@ -1,44 +1,45 @@
-# yay -S bash-language-server
-# yay -S bat
-# yay -S brave-bin
-# yay -S chromium
-# yay -S colordiff
-# yay -S convmv
-# yay -S dictd
-# yay -S discord
-# yay -S docker
-# yay -S docker-compose
-# yay -S dog
-# yay -S dos2unix
-# yay -S dosfstools
-# yay -S dropbox-cli
-# yay -S encfs
-# yay -S evince
-# yay -S feh
-# yay -S flameshot
-# yay -S gbt
-# yay -S gedit
-# yay -S gitg
-# yay -S jhead
-# yay -S jq
-# yay -S lftp
-# yay -S lshw
-# yay -S lsof
-# yay -S mc
-# yay -S meld
-# yay -S nmap
-# yay -S opera
-# yay -S p7zip
-# yay -S pacman-contrib
-# yay -S perl-rename
-# yay -S pgcli
-# yay -S pmount
-# yay -S postgresql
-# yay -S qbittorrent
-# yay -S ripgrep
-# yay -S ripgrep-all
-# yay -S shellcheck
-# yay -S telegram-desktop
-# yay -S unrar
-# yay -S xclip
-# yay -S zip
+yay -S bash-language-server
+yay -S bat
+yay -S brave-bin
+yay -S chromium
+yay -S colordiff
+yay -S convmv
+yay -S dictd
+yay -S discord
+yay -S docker
+yay -S docker-compose
+yay -S dog
+yay -S dos2unix
+yay -S dosfstools
+yay -S dropbox-cli
+yay -S encfs
+yay -S evince
+yay -S feh
+yay -S flameshot
+yay -S gbt
+yay -S gedit
+yay -S gitg
+yay -S jhead
+yay -S jq
+yay -S lftp
+yay -S lshw
+yay -S lsof
+yay -S mc
+yay -S meld
+yay -S nmap
+yay -S opera
+yay -S p7zip
+yay -S pacman-contrib
+yay -S perl-rename
+yay -S pgcli
+yay -S pmount
+yay -S postgresql
+yay -S qbittorrent
+yay -S ripgrep
+yay -S ripgrep-all
+yay -S shellcheck
+yay -S telegram-desktop
+yay -S unrar
+yay -S xclip
+yay -S zip
+yay -S mongosh mongodb-tools-bin
