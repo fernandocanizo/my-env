94aa84ffd8ff7baa9ce184a9c48f0c44ada1e1ab
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-11-15 06:30:01 -0300

    Update local cd aliases
    - remove old stuff I won't be doing anymore
    - comment stuff I may come back again, but not on this new computer for
      the time being
    - add new cd alias for my new brand and shinny technical blog :P

--- src/borrow/cd.alias.sh
+++ src/borrow/cd.alias.sh
@@ -1,21 +1,13 @@
-alias cd.py4at='cd /my/soft/0.learning/2021.04.23,py4at/'
-alias cd.pcc='cd /my/soft/0.learning/2021.06.21,pcc'
-
-alias cd.neps='cd /my/soft/neps'
-
-alias cd.flc.dev='cd /my/soft/0.my.websites/fernandocanizo.dev'
-
-alias cd.snake='cd /my/soft/0.games/2017.05.16,coding.snake.in.4.minutes.30.seconds.with.gamkedo'
-
-alias cd.javascript.allonge='cd /my/soft/0.learning/0.books/2022.01.01,javascript.allonge/'
-
 # I never recall where does Tor Browser save downloads
-alias cd.torbrowser='cd /home/flc/.local/share/torbrowser/tbb/x86_64/tor-browser/Browser/Downloads'
+# alias cd.torbrowser='cd /home/flc/.local/share/torbrowser/tbb/x86_64/tor-browser/Browser/Downloads'
 
-alias cd.dgb='cd /home/flc/my/diaz-garage-back'
-alias cd.dgf='cd /home/flc/my/diaz-garage-front'
+# alias cd.dgb='cd /home/flc/my/diaz-garage-back'
+# alias cd.dgf='cd /home/flc/my/diaz-garage-front'
 
 # SMC
 alias cd.rocky='cd /home/flc/my/smc/nexus/rocky'
 alias cd.pachuli='cd /home/flc/my/smc/nexus/pachuli'
 alias cd.nexus='cd /home/flc/my/smc/nexus/server'
+
+# Blog
+alias cd.tblog='cd /home/flc/my/tblog'
