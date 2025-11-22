eadcb3ae63d7f74f3c21b50739c3415f9cc88fea
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-11-22 14:26:10 -0300

    More function cleaning and some refactoring

--- src/autoload/functions.sh
+++ src/autoload/functions.sh
@@ -1,62 +1,35 @@
-# vim: set filetype=bash
+# vim: set filetype=bash expandtab tabstop=2 shiftwidth=2 softtabstop=2 smartindent
 
 extract () {
 	# uncompress any known compressed file
-	#
-  if [ -f $1 ] ; then
-    case $1 in
-      *.tar.bz2) tar xvjf $1 ;;
-      *.tar.gz) tar xvzf $1 ;;
-      *.bz2) bunzip2 $1 ;;
-      *.rar) unrar x $1 ;;
-      *.gz) gunzip $1 ;;
-      *.tar) tar xvf $1 ;;
-      *.tbz2) tar xvjf $1 ;;
-      *.tgz) tar xvzf $1 ;;
-      *.zip) unzip $1 ;;
-      *.Z) uncompress $1 ;;
-      *.7z) 7z x $1 ;;
-      *) echo "don't know how to extract '$1'..." ;;
-    esac
-  else
-    echo "'$1' is not a valid file!"
-  fi
+	if [ -f $1 ] ; then
+		case $1 in
+			*.tar.bz2) tar xvjf $1 ;;
+			*.tar.gz) tar xvzf $1 ;;
+			*.bz2) bunzip2 $1 ;;
+			*.rar) unrar x $1 ;;
+			*.gz) gunzip $1 ;;
+			*.tar) tar xvf $1 ;;
+			*.tbz2) tar xvjf $1 ;;
+			*.tgz) tar xvzf $1 ;;
+			*.zip) unzip $1 ;;
+			*.Z) uncompress $1 ;;
+			*.7z) 7z x $1 ;;
+			*) echo "don't know how to extract '$1'..." ;;
+		esac
+	else
+		echo "'$1' is not a valid file!"
+	fi
 }
 
 bashDebug() {
 	# more precise bash debugging, use it like:
 	# bashdebug /path/to/script.sh
-	#
 	env PS4=' ${BASH_SOURCE}:${LINENO}(${FUNCNAME[0]}) ' sh -x $1;
-}
-
-joinArgs() {
-	# join args together using the first one as glue
-	#
-	IFS="$1"
-	shift && echo "$*"
-}
-
-myFunctions () {
-	# return defined functions
-	#
-	set | grep '=()'
-}
-
-repeat () {
-	# repeat commands, usage: repeat <number> <command>
-	#
-	local count="$1" i;
-	shift;
-	for i in $(seq 1 "$count");
-	do
-    eval "$@";
-	done
 }
 
 crontest () {
 	# for testing cronjobs: spits a crontab line with time one minute ahead in the future
-	#
 	date '-d +1 minutes' +'%M %k %d %m *'
 }
 
@@ -75,19 +48,11 @@ col() {
 	awk '{print $'$(echo $* | sed -e 's/ /,$/g')'}';
 }
 
-gitexport() {
-	# copy a local checkout without the .git stuff
-	#
-	[[ -d $1 ]] && echo "Directory exists, remove it please." && exit
-	mkdir -p "$1"
-	git archive master | tar -x -C "$1"
-}
-
 mcd() {
 	# 2015.06.05
 	# I started keeping track of all projects and things started by prefixing
 	# directories with the date, but that made cd-ing into them cumbersome, so this
-	# is a cool alias which will grep the list of directories and get into the
+	# is a cool function which will grep the list of directories and get into the
 	# first one from the results
 
 	# Will clobber mtools mcd command if you have it installed
@@ -99,11 +64,9 @@ mcd() {
 	fi
 }
 
-
 git-span() {
 	# 2015.06.20
 	# report time span for a project
-	#
 	git log | grep '^Date' | tail -n 1
 	git log | grep '^Date' | head -n 1
 }
@@ -112,8 +75,8 @@ find-biggest-dirs () {
 	# find biggest directories on provided path
 
 	if [[ -z $1 ]]; then
-    echo "Please provide a path to search"
-    return 0
+		echo "Please provide a path to search"
+		return 0
 	fi
 
 	/usr/bin/env du --one-file-system --summarize $1/* | /usr/bin/env sort --numeric-sort --reverse
@@ -123,29 +86,29 @@ find-biggest-dirs-h () {
 	# find biggest directories on provided path, with human numbers
 
 	if [[ -z $1 ]]; then
-    echo "Please provide a path to search"
-    return 0
+		echo "Please provide a path to search"
+		return 0
 	fi
 
 	du --human-readable --summarize --one-file-system $1/* | sort --human-numeric-sort --reverse
 }
 
 find-biggest-files () {
-	# find biggest files on provided path
+	# find biggest files on provided path or from root if no path provided
+	# uses `sudo` for searching from root
 
 	if [[ -z $1 ]]; then
-    echo "Please provide a path to search"
-    return 0
+		sudo find / -xdev -type f -printf "%s %p\n" | sort --numeric --reverse | head -n 20 | numfmt --to=iec
+	else
+		find "$1" -xdev -type f -printf "%s %p\n" | sort --numeric --reverse | head -n 20 | numfmt --to=iec
 	fi
-
-	find $1 -type f -printf '%s %p\n' | sort --numeric --reverse
 }
 
-# Open URL of an Archlinux package
 function pkgpage () {
-    url=$(yaourt -Si $1 | grep -E '^URL' | grep -E -o 'https?://[^ ]+')
-    echo "Opening ${url} ..."
-    chromium ${url}
+	# open URL of an Archlinux package
+	url=$(yaourt -Si $1 | grep -E '^URL' | grep -E -o 'https?://[^ ]+')
+	echo "Opening ${url} ..."
+	chromium ${url}
 }
 
 owner () {
@@ -161,22 +124,22 @@ whatelse () {
 
 pretendInstall() {
 	for i in `seq 0 100`; do
-    timeout 1 dialog --gauge "Install..." 6 40 "$i"
+		timeout 1 dialog --gauge "Install..." 6 40 "$i"
 	done
 	reset
 }
 
 pretendGarbage() {
 	while (true) ; do
-    pwgen -ny 72
-    sleep 1
+		pwgen -ny 72
+		sleep 1
 	done
 }
 
 getMovieLength () {
 	# get movie length
 	mplayer -vo null -ao null -frames 0 -identify $1 2> /dev/null |
-    gawk '{FS="="}; /ID_LENGTH/{ H=int($2/3600); M=int(($2-H*3600)/60); S=int($2%60); printf("%02d:%02d:%02d\n", H, M, S)}'
+		gawk '{FS="="}; /ID_LENGTH/{ H=int($2/3600); M=int(($2-H*3600)/60); S=int($2%60); printf("%02d:%02d:%02d\n", H, M, S)}'
 }
 
 removePdfPassword() {
@@ -193,12 +156,8 @@ calc() {
 function cue() {
 	# remind you of something to do later
 	if [[ $# -eq 0 ]]; then
-    echo "Usage: $0 <time> <text string>"
+		echo "Usage: $0 <time> <text string>"
 	else
-    sleep $1 && shift && zenity --title 'Remind that...' --info --text "$*" &
+		sleep $1 && shift && zenity --title 'Remind that...' --info --text "$*" &
 	fi
-}
-
-function path() {
-  echo ${PATH}
 }
