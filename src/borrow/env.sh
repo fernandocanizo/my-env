9ab4687005b8f0b1f8e1b5adb244f178803bc2aa
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2026-02-25 09:45:24 -0300

    Also remove $PATH from `env`
    `env` os for other general things, `$PATH` has its own thing going on.

--- src/borrow/env.sh
+++ src/borrow/env.sh
@@ -1,5 +1,3 @@
-export PATH=~/bin:~/bin/public:~/bin/private:~/Dropbox/bin:~/.local/share/fnm:${PATH}:~/.mix/escripts
-
 export GIT_TEMPLATE_DIR="${XDG_CONFIG_HOME}/git/template"
 
 # man an info paths for brew
