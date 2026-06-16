9013ac7e3e74e8d75c0b506c7829014eed6275f5
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2026-06-16 17:24:33 -0300

    Remove unused Omarchy envs

--- host/common/bash/omarchy/envs.sh
+++
@@ -1,6 +0,0 @@
-# Copied from ~/.local/share/omarchy/default/bash/envs
-# Editor used by CLI
-
-# Duplicated from .config/uwsm/env so SSH works too
-export OMARCHY_PATH=$HOME/.local/share/omarchy
-export PATH=$OMARCHY_PATH/bin:$PATH:$HOME/.local/bin
